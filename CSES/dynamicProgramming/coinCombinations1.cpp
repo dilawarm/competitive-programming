3eb6da3e78d504d1290b7e511805a2f9b24bb34b
Dilawar Mahmood <dilawarm@stud.ntnu.no>
2020-03-06 11:04:12 +0100

    i tossed a coin, but the coin tossed me - multitalentloes

---
+++ CSES/dynamicProgramming/coinCombinations1.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+
+    int n, x; cin >> n >> x;
+    vector<int> c(n), dp(x+1);
+    dp[0] = 1;
+    for (int i = 0; i < n; ++i) cin >> c[i];
+    for (int i = 1; i <= x; ++i) {
+        for (int j = 0; j < n; ++j) {
+            if (i - c[j] >= 0) (dp[i] += dp[i - c[j]]) %= 1000000007;
+        }
+    }
+    cout << dp[x] << endl;
+    return 0;
+}
